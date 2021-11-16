ee1962b804a6d49c2077b0ec0331c7d650790082
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-16 21:54:37 +0800

    PUP Grading system

--- prog1.py
+++ prog1.py
@@ -1,8 +1,3 @@
-#Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
-#Example:
-#Input grade: 87.6
-#Grade/Mark: 1.75
-#Description: Very Good
 def getgradePercentage():
     grade_percentage = input("Enter Grade Percentage: ")
     if grade_percentage == "INC":
@@ -11,11 +6,29 @@ def getgradePercentage():
         print(f"Description: Withdrawn")
     elif grade_percentage == "D":
         print(f"Description: Dropped")
-    
-
+    else:
+        roundUp = round(float(grade_percentage))
+        if roundUp >= 97 and roundUp <= 100:
+            print(f"Grade/Mark: 1.0 \nDescription: Excellent")
+        elif roundUp >= 94 and roundUp <= 96:
+            print(f"Grade/Mark: 1.25 \nDescription: Excellent")
+        elif roundUp >= 91 and roundUp <= 93:
+            print(f"Grade/Mark: 1.50 \nDescription: Very Good")
+        elif roundUp >= 88 and roundUp <= 90:
+            print(f"Grade/Mark: 1.75 \nDescription: Very Good")
+        elif roundUp >= 85 and roundUp <= 87:
+            print(f"Grade/Mark: 2.0 \nDescription: Good")
+        elif roundUp >= 82 and roundUp <= 84:
+            print(f"Grade/Mark: 2.25 \nDescription: Good")
+        elif roundUp >= 79 and roundUp <= 81:
+            print(f"Grade/Mark: 2.50 \nDescription: Satisfactory")
+        elif roundUp >= 76 and roundUp <= 78:
+            print(f"Grade/Mark: 2.75 \nDescription: Satisfactory")
+        elif roundUp == 75:
+            print(f"Grade/Mark: 3.0 \nDescription: Passing")
+        else:
+            print(f"Grade/Mark: 5.0 \nDescriptiion: Failure")
 
 gradePercentage = getgradePercentage()
-
-
-#step 1: ask for the grade percentage
+print("Done")
 

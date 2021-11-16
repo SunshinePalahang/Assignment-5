cc03e7be19dd6ddd54ba764bc4cfc9cb2a936f65
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-16 22:45:21 +0800

    Finding the lowest number

--- prog2.py
+++ prog2.py
@@ -2,6 +2,15 @@ def min_of_3():
     a = int(input("First number: "))
     b = int(input("Second number: "))
     c = int(input("Third number: "))
-    
+    if a < b and a < c:
+       min = a
+    elif b < a and b < c:
+        min = b
+    else:
+        min = c
+    return min
+
+
    
 minimum = min_of_3()
+print(minimum)

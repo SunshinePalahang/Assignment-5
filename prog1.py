7a5f499d61ed2dad3e749f9f30812f81b9f2f2a6
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-16 21:21:49 +0800

    asking for the grade percentage

---
+++ prog1.py
@@ -0,0 +1,14 @@
+#Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
+#Example:
+#Input grade: 87.6
+#Grade/Mark: 1.75
+#Description: Very Good
+def getgradePercentage():
+    grade_percentage = input("Enter Grade Percentage: ")
+
+
+gradePercentage = getgradePercentage()
+
+
+#step 1: ask for the grade percentage
+

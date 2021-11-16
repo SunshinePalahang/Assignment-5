b1b511c20918dc1d76cc3200febd7900cb7ecd7e
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-16 21:13:07 +0800

    Life Stages

---
+++ prog3.py
@@ -0,0 +1,12 @@
+age = int(input("Age: "))
+
+if age > -1 and age <= 12:
+    print("Kid")
+elif age >= 13 and age <= 17:
+    print("Teen")
+elif age == 18:
+    print("Debut")
+else: 
+    print("Adult")
+
+print("Done")
